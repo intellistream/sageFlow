b7d23b499d71eb0c4dc6d46308eed5ac5ba354e0
wrp-wrp <2167306203@qq.com>
2025-03-16 10:53:12 +0800

    Revert "添加 candyFlow 数据流系统执行流程文档和示例脚本"
    This reverts commit a07d2a1f0b1575621cd127d0a51759f3cb51997c.

--- docs/join.md
+++
@@ -1,2 +0,0 @@
-+ 按timestamp_和num分别设置一个上下界， 作为 window
-+ left_record / right_record 存， 到达某个阈值/超时就丢掉前面的东西
--- examples/Streaming/run.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/cmake --build /mnt/d/桌面/Github仓库/candyFlow/build --config Debug --target Streaming_example -j 32 --
-"/mnt/d/桌面/Github仓库/candyFlow/build/bin/Streaming_example"
