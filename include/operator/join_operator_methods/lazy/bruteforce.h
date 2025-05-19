7ba0537aa39afc8f35b48b8de07ea5f7f4e0d1d9
ZeroJustMe <619378845@qq.com>
2025-05-19 15:57:59 +0800

    style: format code in BruteForce join method
    1. format code
    2. add cc manager to join operator (prepare for methods using index)

--- include/operator/join_operator_methods/lazy/bruteforce.h
+++ include/operator/join_operator_methods/lazy/bruteforce.h
@@ -8,8 +8,16 @@ namespace candy {
 class BruteForceLazy : public BaseMethod {
  public:
     ~BruteForceLazy() = default;
-    void Excute(std :: vector<std :: pair<int, std::unique_ptr<VectorRecord>>> &emit_pool, std::unique_ptr<candy::JoinFunction> &joinfuc, std::list<std::unique_ptr<VectorRecord>> &left_records_, std::list<std::unique_ptr<VectorRecord>> &right_records_);
-    void Excute(std :: vector<std :: pair<int, std::unique_ptr<VectorRecord>>> &emit_pool, std::unique_ptr<candy::JoinFunction> &joinfuc, std::unique_ptr<VectorRecord> &data, std::list<std::unique_ptr<VectorRecord>> &records_, int slot);
+    void Excute(std::vector<std::pair<int, std::unique_ptr<VectorRecord>>> &emit_pool,
+                std::unique_ptr<candy::JoinFunction> &joinfuc,
+                std::list<std::unique_ptr<VectorRecord>> &left_records_,
+                std::list<std::unique_ptr<VectorRecord>> &right_records_);
+
+    void Excute(std::vector<std::pair<int, std::unique_ptr<VectorRecord>>> &emit_pool,
+                std::unique_ptr<candy::JoinFunction> &joinfuc,
+                std::unique_ptr<VectorRecord> &data,
+                std::list<std::unique_ptr<VectorRecord>> &records_,
+                int slot);
  private:
 };
 }  // namespace candy
