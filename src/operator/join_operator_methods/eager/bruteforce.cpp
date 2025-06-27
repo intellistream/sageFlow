a79d8185a27abdfac90689ed87003794763e3462
ZeroJustMe <619378845@qq.com>
2025-06-27 17:18:55 +0800

    fix: fix bruteforce join method

--- src/operator/join_operator_methods/eager/bruteforce.cpp
+++ src/operator/join_operator_methods/eager/bruteforce.cpp
@@ -1,4 +1,5 @@
 #include "operator/join_operator_methods/eager/bruteforce.h"
+#include "compute_engine/compute_engine.h"
 
 namespace candy {
 void BruteForceEager::Excute(
@@ -15,10 +16,16 @@ void BruteForceEager::Excute(
   std::unique_ptr<VectorRecord> &data,
   std::list<std::unique_ptr<VectorRecord>> &records,
   int slot) {
+  auto temp_compute_engine = std::make_unique<ComputeEngine>();
   for (auto &rec : records) {
-
-      auto response_rec = Response{ResponseType::Record, std::make_unique<VectorRecord>(*rec)};
-      auto response_data = Response{ResponseType::Record, std::make_unique<VectorRecord>(*data)};
+      auto rec_ptr = std::make_unique<VectorRecord>(*rec);
+      auto data_ptr = std::make_unique<VectorRecord>(*data);
+      auto similarity = temp_compute_engine->Similarity(rec_ptr->data_, data_ptr->data_);
+      if (similarity < join_similarity_threshold_) {
+        continue;
+      }
+      auto response_rec = Response{ResponseType::Record, std::move(rec_ptr)};
+      auto response_data = Response{ResponseType::Record, std::move(data_ptr)};
       auto ret = joinfuc -> Execute(response_rec, response_data);
       auto &ret_record = ret.record_;
       if (ret_record != nullptr) {
--- src/operator/join_operator_methods/lazy/bruteforce.cpp
+++ src/operator/join_operator_methods/lazy/bruteforce.cpp
@@ -1,4 +1,5 @@
 #include "operator/join_operator_methods/lazy/bruteforce.h"
+#include "compute_engine/compute_engine.h"
 
 namespace candy {
 void BruteForceLazy::Excute(
@@ -6,10 +7,17 @@ void BruteForceLazy::Excute(
   std::unique_ptr<candy::JoinFunction> &joinfuc,
   std::list<std::unique_ptr<VectorRecord>> &left_records,
   std::list<std::unique_ptr<VectorRecord>> &right_records) {
+  auto temp_compute_engine = std::make_unique<ComputeEngine>();
   for (auto &left : left_records)
       for (auto &right : right_records) {
-          auto response_left = Response{ResponseType::Record, std::move(left)};
-          auto response_right = Response{ResponseType::Record, std::move(right)};
+          auto left_ptr = std::make_unique<VectorRecord>(*left);
+          auto right_ptr = std::make_unique<VectorRecord>(*right);
+          auto similarity = temp_compute_engine->Similarity(left_ptr->data_, right_ptr->data_);
+          if (similarity < join_similarity_threshold_) {
+            continue;
+          }
+          auto response_left = Response{ResponseType::Record, std::move(left_ptr)};
+          auto response_right = Response{ResponseType::Record, std::move(right_ptr)};
           auto result = joinfuc -> Execute(response_left, response_right);
           auto ret_record = std::make_unique<VectorRecord>(*result.record_);
           if (ret_record != nullptr) {
